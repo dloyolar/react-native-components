927f3a5396cd2252a4d047b3f6f0fac49916d717
dloyolar <diego.loyola@continuumhq.com>
2022-07-31 18:22:47 -0400

    refactor: add switch to screen component

--- src/screens/SwitchScreen.tsx
+++ src/screens/SwitchScreen.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, Platform} from 'react-native';
+import {Switch} from 'react-native-gesture-handler';
+import {HeaderTitle} from '../components/HeaderTitle';
 
 export const SwitchScreen = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(!isEnabled);
+
   return (
     <View style={styles.container}>
-      <Text>SwitchScreen</Text>
+      <HeaderTitle title="Switches" />
+      <Switch
+        trackColor={{false: '#D9D9DB', true: '#5856D6'}}
+        thumbColor={Platform.OS === 'android' ? '#5856D6' : ''}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 100,
+    marginHorizontal: 20,
   },
 });
