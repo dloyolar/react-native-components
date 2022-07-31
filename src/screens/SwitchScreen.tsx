49926adf61d44d48216334207905188071fa2c22
dloyolar <diego.loyola@continuumhq.com>
2022-07-31 17:25:52 -0400

    feat: add new screen to navigate

---
+++ src/screens/SwitchScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+export const SwitchScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>SwitchScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 100,
+  },
+});
