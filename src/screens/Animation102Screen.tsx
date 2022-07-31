34f41e574b2cfdf579888f548c7df9fa1959511d
dloyolar <diego.loyola@continuumhq.com>
2022-07-31 17:19:49 -0400

    feat: add component animation panResponse

--- src/screens/Animation102Screen.tsx
+++ src/screens/Animation102Screen.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Animated, PanResponder, Text} from 'react-native';
+import {useRef} from 'react';
 
 export const Animation102Screen = () => {
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+
+    onPanResponderMove: Animated.event(
+      [
+        null,
+        {
+          dx: pan.x,
+          dy: pan.y,
+        },
+      ],
+      {useNativeDriver: false},
+    ),
+
+    onPanResponderRelease: () => {
+      Animated.spring(pan, {
+        toValue: {x: 0, y: 0},
+        useNativeDriver: false,
+      }).start();
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <View style={styles.purpleBox} />
+      <Animated.View
+        {...panResponder.panHandlers}
+        style={[pan.getLayout(), styles.purpleBox]}
+      />
+      <Text>Grab and move the box!</Text>
     </View>
   );
 };
@@ -12,9 +41,11 @@ export const Animation102Screen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   purpleBox: {
-    backgroundColor: 'red',
+    backgroundColor: '#75CEDB',
     width: 150,
     height: 150,
   },
