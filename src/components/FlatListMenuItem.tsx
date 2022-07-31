6ef5171ffc4b06c97783352bc1e8115b26b30aee
dloyolar <diego.loyola@continuumhq.com>
2022-07-31 16:24:11 -0400

    refactor: add touchable with navigation

--- src/components/FlatListMenuItem.tsx
+++ src/components/FlatListMenuItem.tsx
@@ -3,15 +3,19 @@ import {View, Text, StyleSheet} from 'react-native';
 import {MenuItem} from '../inferfaces/appInterfaces';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {TouchableOpacity} from 'react-native-gesture-handler';
+import {useNavigation} from '@react-navigation/native';
 
 interface Props {
   menuItem: MenuItem;
 }
 
 export const FlatListMenuItem = ({menuItem}: Props) => {
+  const navigation = useNavigation();
   const {icon, name} = menuItem;
   return (
-    <TouchableOpacity activeOpacity={0.7}>
+    <TouchableOpacity
+      activeOpacity={0.7}
+      onPress={() => navigation.navigate(menuItem.component as any)}>
       <View style={styles.container}>
         <Icon name={icon} size={23} />
         <Text style={styles.itemText}>{name}</Text>
